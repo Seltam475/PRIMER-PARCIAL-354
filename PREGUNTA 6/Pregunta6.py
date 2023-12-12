faee87a14a6a42581d36ead4967c942dffb03fe1
Seltam475 <115035889+Seltam475@users.noreply.github.com>
2023-12-12 14:21:18 -0400

    Pregunta6.py

--- PREGUNTA 6/Pregunta6.py
+++ PREGUNTA 6/Pregunta6.py
@@ -1,3 +1,6 @@
+#El dataset elegido en PYTHON, realice tres tecnicas de preprocesamiento. Explique la razón de aplicar estas técnicas.
+
+
 from sklearn.datasets import fetch_california_housing
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -5,24 +8,24 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.impute import SimpleImputer
 from sklearn.feature_selection import SelectKBest, f_regression
 
-# Cargar el conjunto de datos
+#se carga el conjunto de datos
 data = fetch_california_housing()
 X = data.data
 y = data.target
 
-# Convertir a DataFrame para mayor flexibilidad
+#se convierte a dataframe para mayor flexibilidad
 df = pd.DataFrame(data=X, columns=data.feature_names)
 df['target'] = y
 
-# 1. Manejo de valores faltantes con SimpleImputer
+# 1- manejo de valores faltantes
 imputer = SimpleImputer(strategy='mean')
 df = pd.DataFrame(imputer.fit_transform(df), columns=df.columns)
 
-# 2. Escalado de características con StandardScaler
+# 2- mscalado de característica
 scaler = StandardScaler()
 df[data.feature_names] = scaler.fit_transform(df[data.feature_names])
 
-# 3. División de datos en conjuntos de entrenamiento y prueba
+# 3- División de datos en conjuntos de entrenamiento y prueba
 X_train, X_test, y_train, y_test = train_test_split(X_selected, df['target'], test_size=0.2, random_state=42)
 
 print(data)
