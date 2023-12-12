414a444f60788f97f1f1f1cc27dfddc184af165a
Seltam475 <115035889+Seltam475@users.noreply.github.com>
2023-12-12 14:15:12 -0400

    Pregunta3.py

--- PREGUNTA 3/Pregunta3.py
+++ PREGUNTA 3/Pregunta3.py
@@ -11,14 +11,17 @@ import numpy as np
 # Encuentra las columnas con valores NaN
 col_nan = np.isnan(data).any(axis=0)
 
-# Imputación por columna (usando la media en este caso)
+# Imputación por columna
 for i in range(data.shape[1]):
     if col_nan[i]:
         # Calcula la media de la columna i
+        
         media_columna = np.nanmean(X[:, i])
+        
         # Encuentra las posiciones con NaN en la columna i
         nan_indices = np.isnan(data[:, i])
-        # Reemplaza los NaN con la media de esa columna
+        
+        # Reemplazo de los NaN con la media de esa columna
         data[nan_indices, i] = media_columna
 
 print(data)
